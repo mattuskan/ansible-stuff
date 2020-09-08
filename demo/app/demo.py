30d43c734284bf4ffe980fadeea7baf1ca414f5e
mattuskan <33846881+mattuskan@users.noreply.github.com>
2020-09-08 10:27:11 -0400

    change hostname to ip

--- demo/app/demo.py
+++ demo/app/demo.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
 db = SQLAlchemy(app)
 hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
 
 @app.route('/')
 def index():
-  return 'Hello, from sunny %s!\n' % hostname
+  return 'Hello, from sunny %s!\n' % ip_address
 
 @app.route('/db')
 def dbtest():
@@ -17,7 +18,7 @@ def dbtest():
       db.create_all()
   except Exception as e:
       return e.message + '\n'
-  return 'Database Connected from %s!\n' % hostname
+  return 'Database Connected from %s!\n' % ip_address
 
 if __name__ == '__main__':
   app.run()
